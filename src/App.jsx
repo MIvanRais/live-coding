09c34b28f05db3f572082df2d931bec6aead4cf1
MIvanRais <26ivanrais@gmail.com>
2024-11-06 20:42:09 +0700

    Change class Attribute

--- src/App.jsx
+++ src/App.jsx
@@ -16,26 +16,32 @@ function App() {
 
   return (
     <>
-      <section class="mx-auto grid h-full w-[1280px] grid-cols-4">
+      <section className="mx-auto grid h-full w-[1280px] grid-cols-4">
         <header
-          class="col-span-4 text-center font-bold text-white"
+          className="col-span-4 text-center font-bold text-white"
           style={{ backgroundColor: color }}
           onClick={handleChangeColor}
         >
           Header
         </header>
-        <main class="col-span-4 row-span-3 grid grid-cols-subgrid">
-          <aside class="row-span-2 bg-orange-400 font-bold text-white">
+        <main className="col-span-4 row-span-3 grid grid-cols-subgrid">
+          <aside className="row-span-2 bg-orange-400 font-bold text-white">
             Aside
           </aside>
-          <section class="bg-amber-950 font-bold text-white">Content 1</section>
-          <section class="bg-amber-500 font-bold text-white">Content 2</section>
-          <section class="bg-lime-400 font-bold text-white">Content 3</section>
-          <section class="col-span-3 bg-green-500 font-bold text-white">
+          <section className="bg-amber-950 font-bold text-white">
+            Content 1
+          </section>
+          <section className="bg-amber-500 font-bold text-white">
+            Content 2
+          </section>
+          <section className="bg-lime-400 font-bold text-white">
+            Content 3
+          </section>
+          <section className="col-span-3 bg-green-500 font-bold text-white">
             Content 4
           </section>
         </main>
-        <footer class="col-span-4 bg-orange-500 font-bold text-white">
+        <footer className="col-span-4 bg-orange-500 font-bold text-white">
           Footer
         </footer>
       </section>
